0c3bab9558bf36325c3fd3257b8fe2a5513ab3fc
Shane Cavanaugh <shane@shanecav.net>
2019-08-19 08:44:55 -0400

    Add activity_pub.inbox.get endpoint

--- lib/activity_streams/factory.rb
+++ lib/activity_streams/factory.rb
@@ -71,7 +71,7 @@ module ActivityStreams
 
     def unsupported_properties(klass, attrs)
       attrs.each.with_object({}) do |(k, _v), unsupported_props|
-        if !klass.properties.include?(k)
+        if !klass.properties.include?(k.to_sym)
           unsupported_props[k] = attrs.delete(k)
         end
       end
