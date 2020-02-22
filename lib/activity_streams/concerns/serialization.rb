95e0d40cfff57d876491d0510c835fa96175f524
Shane Cavanaugh <shane@shanecav.net>
2020-02-22 16:32:03 -0700

    Better avoid stackoverflow errors

--- lib/activity_streams/concerns/serialization.rb
+++ lib/activity_streams/concerns/serialization.rb
@@ -14,37 +14,25 @@ module ActivityStreams
       end
 
       def to_json(*args)
-        JSON.dump(to_h)
+        to_h.
+          transform_values { |v| convert_to_json(v) }.
+          to_json
       end
 
       def to_h
-        props = properties.dup
-        props.merge!(:@context => self[:@context]) if self[:@context]
-        props.merge!(type: self[:type]) if self[:type]
-        props.merge!(id: self[:id]) if self[:id]
-        props.merge!(_unsupported_properties) unless _unsupported_properties.empty?
-        props.transform_keys! { |k| k.to_sym }
-        props = self.is_a?(ActivityStreams::Activity::Update) ? props : props.compact
+        properties.dup
+      end
 
-        # Avoid infinite loops caused by circles in nested properties
-        props = compress.properties
+      private
 
-        ActivityStreams::Utilities::Queue.new.call(props, depth: 1) do |prps|
-          next unless prps.is_a?(Hash)
-
-          queued_up = []
-
-          prps.transform_values! do |v|
-            case v
-            when ActivityStreams::Model then v.properties
-            when Array then v.map { |i| i.to_json }
-            when Date, Time then v.iso8601
-            when OpenStruct then v.to_h
-            else v
-            end
-          end
-
-          queued_up
+      def convert_to_json(v)
+        case v
+        when ActivityStreams then v.to_h
+        when Array then v.map { |vv| convert_to_json(vv) }
+        when Date, Time then v.iso8601
+        when OpenStruct
+          v.to_h.select { |k, _v| !%i[_parent _unsupported_types].include?(k) }
+        else v
         end
       end
     end
