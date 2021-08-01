a4f9cecb7f4b24cb921ec1611d3738e6e0bddacd
Shane Cavanaugh <shane@shanecav.net>
2021-07-31 21:50:44 -0600

    Convert ActivityStreams to their IDs at depth

--- lib/activity_streams/concerns/serialization.rb
+++ lib/activity_streams/concerns/serialization.rb
@@ -14,9 +14,31 @@ module ActivityStreams
       end
 
       def to_json(*args)
-        to_h.
-          transform_values { |v| convert_to_json(v) }.
-          to_json
+        initial = to_h
+        count = 0
+        result = Utilities::Queue.new.call(initial, depth: 10) do |child|
+          count += 1
+
+          if count != 10
+            child.transform_values! { |v| convert_to_json(v) }
+          else
+            # When we reach depth, convert the ActivityStreams objects to their ID strings.
+            child.transform_values! do |v|
+              case v
+              when ActivityStreams then v[:id]
+              when Hash then v.transform_values! do |vv|
+                case vv
+                when ActivityStreams then v[:id]
+                else vv
+                end
+              end
+              else convert_to_json(v)
+              end
+            end
+          end
+        end
+
+        result.to_json
       end
 
       def to_h
